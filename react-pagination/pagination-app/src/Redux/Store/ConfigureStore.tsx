3289aca0b3b919d23ac94977ee54cae9a579f205
rathodrahul05 <rahul.rathod@green-apex.com>
2021-10-26 17:23:02 +0530

    modified first practical

--- react-pagination/pagination-app/src/Redux/Store/ConfigureStore.tsx
+++ react-pagination/pagination-app/src/Redux/Store/ConfigureStore.tsx
@@ -1,6 +1,7 @@
 import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
-import paginationReducer from "../Redux/Reducers/paginationReducer";
+import paginationReducer from "../Reducers/paginationReducer";
+
 
 const store=createStore(paginationReducer,applyMiddleware(thunk));
 export default store;
